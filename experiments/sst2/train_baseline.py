e7b9a6c8c6a3373f6a9c625eb9b5270d8a8c9d84
Paul Gladkov <gladkov.p@gmail.com>
2019-11-22 21:00:47 +0300

    lstm

---
+++ experiments/sst2/train_baseline.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from __future__ import unicode_literals, print_function
+import pandas as pd
+from knowledge_distillation.distilling_lstm import LSTMBaseline
+from knowledge_distillation.utils import get_logger
+
+
+settings = {
+    'max_seq_length': 32,
+    'num_train_epochs': 10,
+    'train_batch_size': 32,
+    'eval_batch_size': 32,
+}
+
+
+if __name__ == '__main__':
+
+    logger = get_logger()
+
+    train_df = pd.read_csv('./.data/sst/train.csv', encoding='utf-8')
+    X_train = train_df['text']
+    y_train = train_df['label']
+
+    trainer = LSTMBaseline(settings, logger)
+    model, vocab = trainer.train(X_train, y_train, y_train, '/app/.data/sst')
+
+    logger.info('validate')
+
+    test_df = pd.read_csv('./.data/sst/test.csv', encoding='utf-8')
+    X_test = test_df['text']
+    y_test = test_df['label']
+
+    trainer.validate(X_test, y_test, model, vocab)
+
---
+++ experiments/sst2/utils.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+
+from __future__ import unicode_literals, print_function
+
+import sys
+import re
+import inflect
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
+
+
+def remove_punctuation(words):
+    """Remove punctuation from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+
+def remove_stopwords(words):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+
+def stem_words(words):
+    """Stem words in list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+
+def normalize(words):
+    words = remove_punctuation(words)
+    words = replace_numbers(words)
+    words = remove_stopwords(words)
+    words = stem_words(words)
+    words = lemmatize_verbs(words)
+    return words
